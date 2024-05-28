57564a3ceb3c47caf3857378a5ded32554f57302
vladeubanks <103177221+Vladdddy@users.noreply.github.com>
2024-05-28 18:11:06 +0200

    Creating repository

---
+++ script.js
@@ -0,0 +1,44 @@
+window.addEventListener('scroll', function () {
+    var competencies = document.getElementById('competencies');
+    var position = competencies.getBoundingClientRect().top;
+
+    if (position < window.innerHeight && position > 0) {
+        competencies.classList.add('show');
+    } else if (position < window.inner) {
+        competencies.classList.remove('show');
+    }
+});
+
+window.addEventListener('scroll', function () {
+    var skills = document.getElementById('skills');
+    var position = skills.getBoundingClientRect().top;
+
+    if (position < window.innerHeight && position > 0) {
+        skills.classList.add('show');
+    } else if (position < window.inner) {
+        skills.classList.remove('show');
+    }
+});
+
+window.addEventListener('scroll', function () {
+    var certificates_carousel = document.getElementById('certificates_carousel');
+    var position = certificates_carousel.getBoundingClientRect().top;
+
+    if (position < window.innerHeight && position > 0) {
+        certificates_carousel.classList.add('show');
+    } else if (position < window.inner) {
+        certificates_carousel.classList.remove('show');
+    }
+});
+
+window.addEventListener('scroll', function () {
+    var graph = document.getElementById('graph');
+    var position = graph.getBoundingClientRect().top;
+
+    if (position < window.innerHeight && position > 0) {
+        graph.classList.add('show');
+    } else if (position < window.inner) {
+        graph.classList.remove('show');
+    }
+});
+
